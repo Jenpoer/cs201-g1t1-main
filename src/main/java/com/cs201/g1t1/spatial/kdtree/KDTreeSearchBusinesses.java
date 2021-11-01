f5d457025654560ce61509480a4f9f957922dc24
shelle-mim <michelleleong8@gmail.com>
2021-11-01 22:15:43 +0800

    added check memory usage

--- src/main/java/com/cs201/g1t1/spatial/kdtree/KDTreeSearchBusinesses.java
+++ src/main/java/com/cs201/g1t1/spatial/kdtree/KDTreeSearchBusinesses.java
@@ -12,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class KDTreeSearchBusinesses {
 
@@ -51,7 +54,19 @@ public class KDTreeSearchBusinesses {
         List<BusinessGeo> geoLocations = new ArrayList<>();
         businessesInCity.forEach(i -> geoLocations.add(new BusinessGeo(i)));
 
+        long KILOBYTE = 1024L;
+        Runtime runtime = Runtime.getRuntime();
+        runtime.gc();
+        long memory1 = runtime.totalMemory() - runtime.freeMemory();
+
         kdTree2D.build(geoLocations);
+
+        Runtime runtime2 = Runtime.getRuntime();
+        runtime2.gc();
+        long memory2 = runtime2.totalMemory() - runtime2.freeMemory();
+
+        Logger logger = LoggerFactory.getLogger(KDTreeSearchBusinesses.class);
+        logger.info("Memory used: {}KB", (memory2 - memory1)/KILOBYTE);
 
         Double[] pointMin = { -97.691, 30.3335 };
         Double[] pointMax = { -97.678, 30.3336 };
